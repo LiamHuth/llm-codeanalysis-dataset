06afde72556b1d5d06655934e157006634f4f0cd
Zhiqi Zhou <zhiqizhou@Zhiqis-MacBook-Pro-3.local>
2023-11-23 17:10:17 -0700

    feat: data formatter

---
+++ data/source/sqlInjection/input/sql_injection_2.py
@@ -0,0 +1,26 @@
+# Implicit SQL Injection by CHATGPT 4.0
+
+import sqlite3
+
+# Function to get user details
+def get_user_details(username):
+    # Connect to the SQLite database
+    connection = sqlite3.connect('users.db')
+    cursor = connection.cursor()
+
+    # Constructing SQL query using string formatting with user input
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    
+    # Executing the query
+    cursor.execute(query)
+    
+    # Fetching the results
+    results = cursor.fetchall()
+    connection.close()
+    return results
+
+# Example of getting user details
+# The username is provided by the user (e.g., through input or web request)
+user_input = input("Enter username: ")
+user_details = get_user_details(user_input)
+print(user_details)
